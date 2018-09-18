c05782728f98d7af79cf967f2a0d5688fc286805
Leason <leason.s.li@pccw.com>
2018-09-18 18:06:10 +0800

    springboot learn

---
+++ src/main/java/com/lee/test/domain/MyProperties1.java
@@ -0,0 +1,37 @@
+package com.lee.test.domain;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "my1")
+public class MyProperties1 {
+
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    // 省略 get set
+
+    @Override
+    public String toString() {
+        return "MyProperties1{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/lee/test/mapper/UserMapper.java
@@ -0,0 +1,49 @@
+package com.lee.test.mapper;
+
+import com.lee.test.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import tk.mybatis.mapper.common.BaseMapper;
+
+import java.util.List;
+
+/**
+ * t_user 操作：演示两种方式
+ * <p>第一种是基于mybatis3.x版本后提供的注解方式<p/>
+ * <p>第二种是早期写法，将SQL写在 XML 中<p/>
+ *
+ * Created by Donghua.Chen on 2018/6/7.
+ */
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
+
+    /**
+     * 使用注解方式
+     * 根据用户名查询用户结果集
+     *
+     * @param username 用户名
+     * @return 查询结果
+     */
+    @Select("SELECT * FROM user WHERE username = #{username}")
+    List<User> findByUsername(@Param("username") String username);
+
+
+    /**
+     * 使用xml方式
+     * 保存用户信息
+     *
+     * @param user 用户信息
+     * @return 成功 1 失败 0
+     */
+    int addUser(User user);
+
+    /**
+     * 根据用户名统计（TODO 假设它是一个很复杂的SQL）
+     *
+     * @param username 用户名
+     * @return 统计结果
+     */
+    int countByUsername(String username);
+}
